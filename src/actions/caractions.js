2f01efa8b4c91353d8b85988e1b21ac69b7e0631
marvin lewis <marvinlewis01@gmail.com>
2020-03-18 18:28:54 -0700

    almost done

---
+++ src/actions/caractions.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+export const BUY_ITEM = "BUY_ITEM";
+
+export const buyItem = item => {
+    return { type: BUY_ITEM, payload: item }
+  };
+
+export const removeFeature = item => {
+    return { type: REMOVE_FEATURE, payload: item }
+  };
